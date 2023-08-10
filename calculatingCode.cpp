326d790b4fdcc6202ce465521ab6186676a0e7fb
SHRIYA CHOUDHARY <shriya.choudhary9876@gmail.com>
2023-08-10 23:26:36 +0530

    10 aug

--- calculatingCode.cpp
+++ calculatingCode.cpp
@@ -1,25 +1,23 @@
 #include<iostream>
 using namespace std;
-float addition(float , float);
-float subtract(float , float);
-float divide(float , float);
-float multiply(float, float);
-void show(float);
-float addition(float x, float y){
-    return(x+y);
+
+class StandardCalc{
+    public:
+    void add();
+    void double sub(double,double);
+
+} 
+
+void StandardCalc::add(double a, double b){
+    return (a+b);
 }
-float subtract(float x, float y){
-    return(x-y);
+
+void StandardCalc::sub(double a,double b){
+    return(a-b;)
 }
-float divide(float x, float y){
-    return(x/y);
-}
-float multiply(float x, float y){
-    return(x*y);
-}
-void show(float c){
-    cout<<"The answer is : "<<c;
-}
+
+
+
 void askType(){
     int type;
     cout<<"\n\n\t\t\t~ ~ ~ ~ TYPES OF CALCULATORS ~ ~ ~ ~\t\t\t\n\n";
@@ -28,30 +26,12 @@ void askType(){
 }
 
 int main(){
-    float a,b,sum,sub,div,mul;
-    char c;
-    cout<<"Enter your first number please : ";
-    cin>>a;
-    cout<<"Enter arithmetic symbol please (+,-,*,/) : ";
-    cin>>c;
-    cout<<"Enter your second number please : ";
-    cin>>b;
+
+    StandardCalc obj1;
     
-    switch(c){
-    case '+':
-    sum=addition(a,b);
-    show(sum);
-    break;
-    case '-': 
-    sub=subtract(a,b);
-    show(sub);
-    break;
-    case '/':
-    div=divide(a,b);
-    show(div);
-    case '*' :
-    mul=multiply(a,b);
-    show(mul);
-    }
+    askType();
+    if type=1
+
+    obj1.add
 
 }
