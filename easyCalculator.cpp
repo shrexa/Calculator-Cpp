29116f38d68cdc6aacdaec706b4397cb337a14db
SHRIYA CHOUDHARY <shriya.choudhary9876@gmail.com>
2023-08-10 23:09:48 +0530

    functns added

--- easyCalculator.cpp
+++ easyCalculator.cpp
@@ -20,3 +20,31 @@ float multiply(float x, float y){
 void show(float c){
     cout<<"The answer is : "<<c;
 }
+int main(){
+    float a,b,sum,sub,div,mul;
+    char c;
+    cout<<"Enter your first number please : ";
+    cin>>a;
+    cout<<"Enter arithmetic symbol please (+,-,*,/) : ";
+    cin>>c;
+    cout<<"Enter your second number please : ";
+    cin>>b;
+    
+    switch(c){
+    case '+':
+    sum=addition(a,b);
+    show(sum);
+    break;
+    case '-': 
+    sub=subtract(a,b);
+    show(sub);
+    break;
+    case '/':
+    div=divide(a,b);
+    show(div);
+    case '*' :
+    mul=multiply(a,b);
+    show(mul);
+    }
+
+}
