096d5d74c86ca7e11a892be05caa67de3aabe8b2
SHRIYA CHOUDHARY <shriya.choudhary9876@gmail.com>
2023-08-13 00:06:09 +0530

    12aug

--- calculatingCode.cpp
+++ calculatingCode.cpp
@@ -54,7 +54,7 @@ int main(){
         cout<<"1.Addition\n2.Subraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Go Back\n\nEnter your Choice : ";
         int t;
         cin>>t;
-        void clrscr();
+        // void clrscr();
         switch (t)
         {
         case 1:
@@ -70,7 +70,26 @@ int main(){
     else if (type='2')
     {
         cout<<"\n\n\t\t ~ ~ ~ Operations for Scientific Calculator~ ~ ~ \t\t\n\n";
-        cout<<"1.Sqaure Root\n2.Square\n3.Cube\n4.Power\n5.Factorial\n6.Sine function\n7.Cos Function\n8.Tan Function\n6.Go Back\n";
+        cout<<"1.Sqaure Root\n2.Square\n3.Cube\n4.Power\n5.Factorial\n6.Sine function\n7.Cos Function\n8.Tan Function\n9.Go Back\n\nEnter your Choice : ";
+        int n;
+        cin>>n;
+        // void clrscr();
+        switch (n)
+        {
+        case 1:
+            cout<<"power lga";
+            break;
+        case 2:
+             break;
+        case 3:
+             break;
+        case 4:
+             break;
+        
+        default:
+        cout<<"\nPlease Enter a valid choice no.! ( between 1 to 9 only) ";
+            break;
+        }
 
     } else{
         cout<<"\nPlease Enter 1 or 2 only ! ";
