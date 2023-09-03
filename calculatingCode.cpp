739264404e3c2b9dd08df75ed387c690d2d86cd8
SHRIYA CHOUDHARY <shriya.choudhary9876@gmail.com>
2023-09-03 17:28:54 +0530

    completed

--- calculatingCode.cpp
+++ calculatingCode.cpp
@@ -17,22 +17,23 @@ public:
 class ScientificCalc
 {
 public:
-    static double Sqt(double);
+    static double sqt(double);
     static double sq(double);
-    static double cub(double, double);
-    static double powr(double, double);
-    static double fact(double, double);
+    static double cub(double);
+    // static double powr(double, double);
+    // static double fact(double, double);
 };
-double ScientificCalc::Sqt(double a)
+double ScientificCalc::sqt(double a)
 {
     double root1 = sqrt(a);
     // sqrt is a keyword
     return root1;
 }
-double ScientificCalc::Sq(double a)
+double ScientificCalc::sq(double a)
 {
     double num1 = a * a;
     return num1;
+    // return (a*a);
 }
 double ScientificCalc::cub(double a)
 {
@@ -82,7 +83,8 @@ int main()
 
     // void clrscr(void);
     // void askType(int type);
-
+StandardCalc obj1;
+ScientificCalc obj2;
     // askType;
 
     cout << "\n\n\t\t\t ~ ~ ~ ~ TYPES OF CALCULATORS ~ ~ ~ ~ \t\t\t\n\n";
@@ -106,12 +108,17 @@ int main()
 
     if (type == 1)
     {
-        StandardCalc obj1;
+        
         cout << "\n\n\t\t ~ ~ ~ Operations for Standard Calculator ~ ~ ~ \t\t\n\n";
         cout << "1.Addition\n2.Subraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Go Back\n\nEnter your Choice : ";
         int t;
         cin >> t;
         // void clrscr();
+        
+         
+        cout<<"\n\n";
+        
+        
         switch (t)
         {
         case 1:
@@ -187,19 +194,46 @@ int main()
     else if (type == 2)
     {
         cout << "\n\n\t\t ~ ~ ~ Operations for Scientific Calculator~ ~ ~ \t\t\n\n";
-        cout << "1.Sqaure Root\n2.Square\n3.Cube\n4.Power\n5.Factorial\n6.Sine function\n7.Cos Function\n8.Tan Function\n9.Go Back\n\nEnter your Choice : ";
+        cout << "1.Sqaure Root\n2.Square\n3.Cube\n4.Power\n5.Factorial\n6.Sine function\n7.Cos Function\n8.Tan Function\n9.Go Back\n\nEnter yourChoice : ";
         int n;
         cin >> n;
         // void clrscr();
+        
+        cout<<"\n\n";
+        
         switch (n)
         {
         case 1:
-            cout << "power lga";
+         {
+             double n1;
+            cout << "Enter the number : ";
+            cin >> n1;
+            double ans1;
+            ans1 = obj2.sqt(n1);
+            cout << "The square root of " << n1 << " is " << ans1 << endl;
             break;
+        }
         case 2:
+        {
+            double n1;
+            cout << "Enter the number : ";
+            cin >> n1;
+            double ans1;
+            ans1 = obj2.sq(n1);
+            cout << "The square of " << n1 << " is " << ans1 << endl;
             break;
+        }
         case 3:
-            break;
+        {
+            double n1;
+            cout << "Enter the number : ";
+            cin >> n1;
+            double ans1;
+            ans1 = obj2.cub(n1);
+            cout << "The cube of " << n1 << " is " << ans1 << endl;
+        }
+         break;
+         
         case 4:
             double base, exponent, result;
             cout << "Enter the base: ";
@@ -210,8 +244,9 @@ int main()
             // using the pow() function from cmath library
             cout << base << " raised to the power of " << exponent << " is " << result << endl;
             break;
-        case 5;
-            int num, factorial = 1;
+        case 5:
+            int num;
+            int factorial = 1;
             cout << "Enter a positive integer: ";
             cin >> num;
             for (int i = 1; i <= num; i++) {
@@ -219,10 +254,9 @@ int main()
             } cout
             << "The factorial of " << num << " is " << factorial << endl;
             break;
-
-            default:
-            cout << "\nPlease Enter a valid choice no.! ( between 1 to 9 only) ";
-            break;
+        // default:
+        //     cout << "\nPlease Enter a valid choice no.! ( between 1 to 9 only) ";
+        //     break;
         }
     }
     else
